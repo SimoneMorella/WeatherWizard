fca9ca841dfadbc9fbcd1ea708db5766fd10c5a1
SimoneMorella <simonemorella21@gmail.com>
2024-05-20 03:20:10 +0200

    changed bg-color and added searchbar

--- Weather-App/tailwind.config.js
+++ Weather-App/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+       "bg-pattern": "url('src/img/bg-app.jpg')"
+      }
+    },
+    fontFamily: {
+      'nunito': ["Nunito", 'san-serif']
+    }
   },
   plugins: [],
 }
